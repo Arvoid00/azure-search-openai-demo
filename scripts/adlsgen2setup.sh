83da7c123f04921e4be9f0bf8d08526c38de52a9
Arvid de Haas <arviddehaas@hotmail.nl>
2024-03-05 19:29:39 +0000

    data ingestion edits

--- scripts/adlsgen2setup.sh
+++ scripts/adlsgen2setup.sh
@@ -2,11 +2,12 @@
 
 . ./scripts/loadenv.sh
 
-if [ -n "$AZURE_ADLS_GEN2_STORAGE_ACCOUNT" ]; then
+if [ -z "$AZURE_ADLS_GEN2_STORAGE_ACCOUNT" ]; then
     echo 'AZURE_ADLS_GEN2_STORAGE_ACCOUNT must be set to continue'
+    echo $AZURE_ADLS_GEN2_STORAGE_ACCOUNT
     exit 1
 fi
 
 echo 'Running "adlsgen2setup.py"'
 
-./scripts/.venv/bin/python ./scripts/adlsgen2setup.py './data/*' --data-access-control './scripts/sampleacls.json' --storage-account "$AZURE_ADLS_GEN2_STORAGE_ACCOUNT" -v
+./scripts/.venv/bin/python ./scripts/adlsgen2setup.py './data/' --data-access-control './scripts/sampleacls.json' --storage-account "$AZURE_ADLS_GEN2_STORAGE_ACCOUNT" -v
